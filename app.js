716f71c1309f2dc21ac83398d15ebc14e57adc54
sunagi <akaractionend@icloud.com>
2023-12-09 21:42:46 +0900

    first

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
 const express = require('express');
+const admin = require('firebase-admin');
 const app = express();
-// const port = 3000;
+const firebaseConfig = require('firebaseConfig');
 
-// ルーティングの設定
-const indexRouter = require('./routes/index');
+// Firebaseの初期化
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseConfig),
+  databaseURL: firebaseConfig.databaseURL
+});
+
+// Firebase Realtime Databaseのインスタンスを取得
+const db = admin.database();
+
+// ボディパーサーの設定（フォームデータを解析するために必要）
+app.use(express.urlencoded({ extended: true }));
 
 // 静的ファイルのためのディレクトリを指定
 app.use(express.static('public'));
 
-// ホームページのルートを使用
-app.use('/', indexRouter);
-
-// リダイレクトロジック
-app.get('/redirect', (req, res) => {
-    const userAgent = req.headers['user-agent'];
-
-    let redirectUrl;
-
-    if (/iPhone|iPad|iPod/i.test(userAgent)) {
-        // iOSデバイスの場合のリダイレクトURL
-        redirectUrl = 'https://example.com/ios';
-    } else if (/Android/i.test(userAgent)) {
-        // Androidデバイスの場合のリダイレクトURL
-        redirectUrl = 'https://example.com/android';
-    } else {
-        // その他のデバイスの場合のリダイレクトURL
-        redirectUrl = 'https://example.com/other';
-    }
-
-    res.redirect(redirectUrl);
+// アンケートページへのルーティング
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/survey.html');
 });
 
-// サーバーを起動
-// app.listen(port, () => {
-//     console.log(`Server running on http://localhost:${port}`);
-// });
+// アンケート送信後の処理
+app.post('/submit-survey', (req, res) => {
+    // フォームからのデータを取得
+    const data = req.body;
+
+    // Firebase Realtime Databaseにデータを保存
+    const ref = db.ref('surveys');
+    ref.push().set(data, (error) => {
+        if (error) {
+            console.error('データの保存に失敗:', error);
+            res.send('エラーが発生しました');
+        } else {
+            // ユーザーエージェントに基づいてリダイレクト
+            const userAgent = req.headers['user-agent'];
+            if (/iPhone|iPad|iPod/i.test(userAgent)) {
+                res.redirect('https://example.com/ios');
+            } else if (/Android/i.test(userAgent)) {
+                res.redirect('https://example.com/android');
+            } else {
+                res.redirect('https://example.com/other');
+            }
+        }
+    });
+});
 
 module.exports = app;
