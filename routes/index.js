7929d9197f05be3457ab976cc3e06ec42faffca3
sunagi <akaractionend@icloud.com>
2023-12-09 23:11:44 +0900

    first

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 // ホームページのルートを設定
 router.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '../views/survey.html'));
 });
 
 module.exports = router;
