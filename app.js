6b7e7b90c900cdcd20f4b0d6161f2fe870b61ed2
sunagi <akaractionend@icloud.com>
2023-12-14 18:52:36 +0900

    first

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
 const express = require('express');
-const admin = require('firebase-admin');
+const AWS = require('aws-sdk');
 const app = express();
 const indexRouter = require('./routes/index');
-const firebaseConfig = require('./firebaseConfig');
+require('dotenv').config();
 
-// Firebaseの初期化
-admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
-  databaseURL: firebaseConfig.databaseURL
+// AWS DynamoDBの設定
+AWS.config.update({
+  region: 'ap-northeast-1', // 適切なリージョンに設定
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-// Firebase Realtime Databaseのインスタンスを取得
-const db = admin.database();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-// ボディパーサーの設定（フォームデータを解析するために必要）
+// ボディパーサーの設定
 app.use(express.urlencoded({ extended: true }));
 
-// 静的ファイルのためのディレクトリを指定
+// 静的ファイルとルーティングの設定
 app.use(express.static('public'));
 app.use('/', indexRouter);
-// アンケートページへのルーティング
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/survey.html');
-});
 
 // アンケート送信後の処理
 app.post('/submit-survey', (req, res) => {
-    // フォームからのデータを取得
     const data = req.body;
+    const params = {
+        TableName: 'YUUUU', // DynamoDBのテーブル名
+        Item: {
+            // ここにDynamoDBに保存するデータ項目を設定
+            'ID': Date.now().toString(), // ユニークなID
+            ...data
+        }
+    };
 
-    // Firebase Realtime Databaseにデータを保存
-    const ref = db.ref('surveys');
-    ref.push().set(data, (error) => {
+    // DynamoDBにデータを保存
+    dynamoDb.put(params, (error, result) => {
         if (error) {
             console.error('データの保存に失敗:', error);
             res.send('エラーが発生しました');
         } else {
-            // ユーザーエージェントに基づいてリダイレクト
+            // リダイレクト処理
             const userAgent = req.headers['user-agent'];
             if (/iPhone|iPad|iPod/i.test(userAgent)) {
                 res.redirect('https://example.com/ios');
