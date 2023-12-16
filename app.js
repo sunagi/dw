a9928837f4e7d346575fc6f1e9fc54c333646c6f
sunagi <akaractionend@icloud.com>
2023-12-16 16:09:54 +0900

    first

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 const app = express();
-const indexRouter = require('./routes/index');
 require('dotenv').config();
 
 // AWS DynamoDBの設定
 AWS.config.update({
-  region: 'ap-northeast-1', // 適切なリージョンに設定
+  region: 'ap-northeast-1',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
@@ -18,17 +17,18 @@ app.use(express.urlencoded({ extended: true }));
 
 // 静的ファイルとルーティングの設定
 app.use(express.static('public'));
-app.use('/', indexRouter);
 
 // アンケート送信後の処理
 app.post('/submit-survey', (req, res) => {
     const data = req.body;
     const params = {
-        TableName: 'YUUUU', // DynamoDBのテーブル名
+        TableName: 'YourTableName', // DynamoDBのテーブル名
         Item: {
-            // ここにDynamoDBに保存するデータ項目を設定
             'ID': Date.now().toString(), // ユニークなID
-            ...data
+            'Gender': data.gender,
+            'Age': data.age,
+            'Location': data.location,
+            'Purpose': data.purpose
         }
     };
 
@@ -38,17 +38,10 @@ app.post('/submit-survey', (req, res) => {
             console.error('データの保存に失敗:', error);
             res.send('エラーが発生しました');
         } else {
-            // リダイレクト処理
-            const userAgent = req.headers['user-agent'];
-            if (/iPhone|iPad|iPod/i.test(userAgent)) {
-                res.redirect('https://example.com/ios');
-            } else if (/Android/i.test(userAgent)) {
-                res.redirect('https://example.com/android');
-            } else {
-                res.redirect('https://example.com/other');
-            }
+            // 保存成功時の処理
+            res.send('アンケートが送信されました');
         }
     });
 });
 
-module.exports = app;
+app.listen(3000, () => console.log('Server running on port 3000'));
